1b563ee87bfeca2f672b0d2e8e2703371716c3b4
CatsMeow492 <calisurf243@hotmail.com>
2022-09-12 18:19:55 -0700

    fs-extra

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
 
 const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery');
