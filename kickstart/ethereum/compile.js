8a644871f3611b120258689507f508134dea0dec
CatsMeow492 <calisurf243@hotmail.com>
2022-09-12 18:32:05 -0700

    improved compile

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -1,5 +1,20 @@
-const path = require('path');
-const fs = require('fs-extra');
-const solc = require('solc');
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
 
-const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery');
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + ".json"),
+    output[contract]
+  );
+}
+
